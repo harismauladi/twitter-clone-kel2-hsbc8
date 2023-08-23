b76371a1be42aa853e76fc46f762b6b6ac5a57ec
DhaniYonata <dhaniyonata21@gmail.com>
2023-08-23 21:04:39 +0700

    desktop display

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,13 @@
 /** @type {import('tailwindcss').Config} */
+
 module.exports = {
   content: ["*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily:{
+        body:['compact']
+      }
+    },
   },
   plugins: [],
 };
